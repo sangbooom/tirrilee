3bfe1b7e18d372b2272f15b3faa56d20cf62dc90
sangbooom <tkdqja4164@naver.com>
2021-03-31 17:15:43 +0900

    chore: 프로젝트 시작

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>assfd</Text>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+export default App;
