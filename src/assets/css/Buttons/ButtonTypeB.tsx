1c05e4ea816ca858e093d7d78e37e7297e343a0e
sangbooom <tkdqja4164@naver.com>
2021-04-03 15:55:40 +0900

    chore: bottom tab navigator에 필요한 이미지 추가

--- src/assets/css/Buttons/ButtonTypeB.tsx
+++ src/assets/css/Buttons/ButtonTypeB.tsx
@@ -8,7 +8,13 @@ export const ButtonTypeB_next__Disabled = styled.TouchableOpacity`
   background-color: #eaeaea;
 `;
 
-export const ButtonTypeB_next__Activation = styled.TouchableOpacity``;
+export const ButtonTypeB_next__Activation = styled.TouchableOpacity`
+  width: 334px;
+  height: 56px;
+  padding: 16px 155px;
+  border-radius: 8px;
+  background-color: #226bef;
+`;
 
 export const ButtonTypeB_finished__Disabled = styled.TouchableOpacity``;
 
