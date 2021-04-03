9294a72fff7cf181c4977a73aa70ad033f302fd0
sangbooom <tkdqja4164@naver.com>
2021-04-03 23:58:43 +0900

    style: 재사용 css 컴포넌트 추가 및 수정

---
+++ src/assets/css/InputForm_B/InputFormB.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/native';
+
+export const InputFormB = styled.TextInput`
+  width: 334px;
+  height: 226px;
+  padding: 14px 12px;
+  border-radius: 8px;
+  border: solid 1px #eaeaea;
+  background-color: #ffffff;
+
+  font-size: 14px;
+  font-weight: normal;
+  line-height: 20px;
+  text-align: left;
+  color: #000000;
+`;
--- src/assets/css/TopNavigation/TopNavigation.tsx
+++ src/assets/css/TopNavigation/TopNavigation.tsx
@@ -10,3 +10,20 @@ export const TopNavigation__back = styled.View`
   padding: 16px 325px 16px 0px;
   background-color: #ffffff;
 `;
+
+export const TopNavigation__logo = styled.View`
+  width: ${deviceWidth}px;
+  height: 56px;
+  margin: 0 0 28px;
+  padding: 18px 0 17px 7px;
+  background-color: #ffffff;
+`;
+
+export const TopNavigation__done = styled.View`
+  width: ${deviceWidth}px;
+  height: 56px;
+  padding: 16px 12px;
+  background-color: #ffffff;
+  flex-direction: row;
+  justify-content: space-between;
+`;
