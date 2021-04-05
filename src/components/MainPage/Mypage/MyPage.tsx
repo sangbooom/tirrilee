c9d8d2c3e75ce3a7425089a821a78ee86ce4a015
sangbooom <tkdqja4164@naver.com>
2021-04-05 21:21:47 +0900

    feat: 마이페이지에서 로그아웃 버튼 추가(임시)

--- src/components/MainPage/Mypage/MyPage.tsx
+++ src/components/MainPage/Mypage/MyPage.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button, Alert} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 interface MyPageProps {}
 
 const MyPage: React.FC<MyPageProps> = () => {
+  const logOutHandler = () => {
+    Alert.alert('경고', '정말 로그아웃 하시겠습니까?', [
+      {text: '확인', onPress: logOut},
+      {text: '취소'},
+    ]);
+  };
+  const logOut = () => {
+    auth().signOut();
+  };
   return (
     <View>
       <Text>MyPage</Text>
+      <Button title="로그아웃" onPress={logOutHandler} />
     </View>
   );
 };
